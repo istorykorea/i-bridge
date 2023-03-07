6d3a793eeba996c1453e395f39d126ead8397e9b
minwook <minkj1992@gmail.com>
2023-03-07 15:12:22 +0900

    docs: v1 dao proposal

--- src/lib/components/home/App.svelte
+++ src/lib/components/home/App.svelte
@@ -1,6 +1,8 @@
 <div class="flex items-center justify-center min-h-screen bg-slate-300">
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
 </div>
+
+<button class="px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 text-white">...</button>
 
 <style>
 </style>
